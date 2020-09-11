6cd0849aa4c281a8467d2a1b449acad9fff74134
Ayushzz <ayushguptadon@gmail.com>
2020-09-11 16:16:03 +0530

    Updates

---
+++ precache-manifest.f7fd0f2b80c7af7d27e53f0de51b8717.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "368f7887822d45ae2ad89a6901afda1b",
+    "url": "/PomodoroClock/index.html"
+  },
+  {
+    "revision": "768f1e1e6f655911492a",
+    "url": "/PomodoroClock/static/css/main.9735328c.chunk.css"
+  },
+  {
+    "revision": "1d156dc978ecc18b4557",
+    "url": "/PomodoroClock/static/js/2.73f98c69.chunk.js"
+  },
+  {
+    "revision": "f5eeca88c65df516af9519a8bb5412f5",
+    "url": "/PomodoroClock/static/js/2.73f98c69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "768f1e1e6f655911492a",
+    "url": "/PomodoroClock/static/js/main.c1314b0b.chunk.js"
+  },
+  {
+    "revision": "8868301e757ef97945a4",
+    "url": "/PomodoroClock/static/js/runtime-main.c765d7f6.js"
+  }
+]);
